cf3b482e92678de3af9ff238fa9ce983117f057a
Justin Vaughn <justin.vaughn@publicissapient.com>
2021-08-30 12:14:27 -0700

    Continuing refactor to atomic design, card size reformatters functional but not styled

--- src/utils/Helpers.ts
+++ src/utils/Helpers.ts
@@ -1,15 +1,24 @@
-export default {
-  cleanString: (str: string): string => str.replace(/\s/g, '_').toLowerCase(),
+// Returns a variant of the original string with spaces replace with underscores
+export const cleanString = (str: string): string => str.replace(/\s/g, '_').toLowerCase();
 
-  capitalizeFirst: (str: string): string => {
-    const arr = str.split('') as string[];
-    if (!arr[0].match(/[a-z]/)) return str;
-    const first = arr.shift()?.toUpperCase();
+// Capitalizes the first letter of a given string
+export const capitalizeFirst = (str: string): string => {
+  const arr = str.split('') as string[];
 
-    if (!first) throw new Error('Error: Invalid string given to helper function CapitalizeFirst');
-    arr.unshift(first);
+  if (!arr[0].match(/[a-z]/)) return str;
+  const first = arr.shift()?.toUpperCase();
 
-    const joined = arr.join('');
-    return joined;
-  },
+  if (!first) throw new Error('Error: Invalid string given to helper function CapitalizeFirst');
+  arr.unshift(first);
+
+  const joined = arr.join('');
+  return joined;
+};
+
+// Capitalizes the first letter of all words seperated by spaces
+export const titleCase = (str: string): string => {
+  const arr = str.split(' ');
+  const replacement = arr.map((word: string) => capitalizeFirst(word));
+  console.log(`In: ${str}   \nOut: ${replacement.join(' ')}`);
+  return replacement.join(' ');
 };
